598aa8c67d4409dd670284af3631e69e9f56994d
amanibensassi <123634459+amanibensassi@users.noreply.github.com>
2024-05-04 16:52:56 +0200

    Update SignupController.java

--- src/main/java/tn/esprit/espritgather/control/SignupController.java
+++ src/main/java/tn/esprit/espritgather/control/SignupController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 
 @RestController
 @RequestMapping("/signup")
